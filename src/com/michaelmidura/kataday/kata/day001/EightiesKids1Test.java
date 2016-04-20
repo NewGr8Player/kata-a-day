2892cc5b1aa82a7090e9cb64f3a361a6a885b1c3
michaelwm <michael@miduras.com>
2016-04-20 17:15:12 -0400

    day 1

---
+++ src/com/michaelmidura/kataday/kata/day001/EightiesKids1Test.java
@@ -0,0 +1,46 @@
+package com.michaelmidura.kataday.kata.day001;
+
+import java.util.Map;
+import java.util.HashMap;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class EightiesKids1Test {
+
+    @Test
+    public void test1() {
+        Map<String, Integer> env = new HashMap<>(2, 1f);
+        env.put("freezing temps", 10);
+        env.put("clear skies", -2);
+        assertEquals("It took 260 licks to get to the tootsie roll center of a tootsie pop. The toughest challenge was freezing temps.", EightiesKids1.totalLicks(env));
+    }
+
+    @Test
+    public void test2() {
+        Map<String, Integer> env = new HashMap<>(2, 1f);
+        env.put("happiness", -5);
+        env.put("clear skies", -2);
+        assertEquals("It took 245 licks to get to the tootsie roll center of a tootsie pop.", EightiesKids1.totalLicks(env));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals("It took 252 licks to get to the tootsie roll center of a tootsie pop.", EightiesKids1.totalLicks(new HashMap<>(0)));
+    }
+
+    @Test
+    public void test4() {
+        Map<String, Integer> env = new HashMap<>(3, 1f);
+        env.put("dragons", 100);
+        env.put("evil wizards", 110);
+        env.put("trolls", 50);
+        assertEquals("It took 512 licks to get to the tootsie roll center of a tootsie pop. The toughest challenge was evil wizards.", EightiesKids1.totalLicks(env));
+    }
+
+    @Test
+    public void test5() {
+        Map<String, Integer> env = new HashMap<>(1, 1f);
+        env.put("white magic", -250);
+        assertEquals("It took 2 licks to get to the tootsie roll center of a tootsie pop.", EightiesKids1.totalLicks(env));
+    }
+}
