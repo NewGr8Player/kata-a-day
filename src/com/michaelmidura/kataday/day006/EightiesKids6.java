ab7fbbd30c2c2fd12c6d650213423888a3ac011b
michaelwm <michael@miduras.com>
2016-04-25 12:43:09 -0400

    day006 setup

---
+++ src/com/michaelmidura/kataday/day006/EightiesKids6.java
@@ -0,0 +1,10 @@
+package com.michaelmidura.kataday.day006;
+
+import java.util.Map;
+
+public class EightiesKids6 {
+
+    public static String fight(Robot robot1, Robot robot2, Map<String, Integer> tactics) {
+        // Which robot will Rock 'em, Sock 'em?!
+    }
+}
---
+++ src/com/michaelmidura/kataday/day006/Robot.java
@@ -0,0 +1,16 @@
+package com.michaelmidura.kataday.day006;
+
+public final class Robot {
+
+    private final String name;
+    private final int health, speed;
+    private final String[] tactics;
+
+    public Robot(String name, int health, int speed, String[] tactics) {
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+        this.tactics = tactics;
+    }
+
+}
