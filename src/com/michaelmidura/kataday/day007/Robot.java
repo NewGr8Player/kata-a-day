f04501531d446ea4ad038376ae5ddcb957903254
michaelwm <michael@miduras.com>
2016-04-26 09:36:39 -0400

    day 7

--- src/com/michaelmidura/kataday/day007/Robot.java
+++ src/com/michaelmidura/kataday/day007/Robot.java
@@ -1,13 +1,21 @@
 package com.michaelmidura.kataday.day007;
 
-import java.util.List;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
 
 public class Robot {
 
-    public List<String> wordBank;
+    public Set<String> wordBank = new HashSet<>(Arrays.asList("thank", "you", "for", "teaching", "me", "i", "already", "know", "the", "word", "do", "not", "understand", "input"));
 
     public String learnWord(String word) {
-        return "";
+        if (Pattern.matches("[a-zA-Z]+", word)) {
+            if (wordBank.add(word.toLowerCase()))
+                return "Thank you for teaching me " + word;
+            return "I already know the word " + word;
+        }
+        return "I do not understand the input";
     }
 
 }
