4dfac0737e1bd05bd557bbd5ac43976342375af2
michaelwm <michael@miduras.com>
2016-04-27 09:45:49 -0400

    day 8

--- src/com/michaelmidura/kataday/day008/TheClockwiseSpiral.java
+++ src/com/michaelmidura/kataday/day008/TheClockwiseSpiral.java
@@ -2,7 +2,54 @@ package com.michaelmidura.kataday.day008;
 
 public class TheClockwiseSpiral {
 
-    public static int[][] createSpiral(int N) {
-        return null;
+    private static final int RIGHT = 0, DOWN = 1, LEFT = 2, UP = 3;
+
+    public static int[][] createSpiral(int n) {
+        int[][] spiral = new int[n][n];
+        int direction = RIGHT, filler = 1, xPos = 0, yPos = 0;
+        while (filler <= (n * n)) {
+            if (direction == RIGHT) {
+                if (xPos < n && spiral[yPos][xPos] == 0) {
+                    spiral[yPos][xPos] = filler++;
+                    xPos++;
+                } else {
+                    yPos++;
+
+                    xPos--;
+                    direction = DOWN;
+                }
+            } else if (direction == DOWN) {
+                if (yPos < n && spiral[yPos][xPos] == 0) {
+                    spiral[yPos][xPos] = filler++;
+                    yPos++;
+                } else {
+                    xPos--;
+
+                    yPos--;
+                    direction = LEFT;
+                }
+            } else if (direction == LEFT) {
+                if (xPos >= 0 && spiral[yPos][xPos] == 0) {
+                    spiral[yPos][xPos] = filler++;
+                    xPos--;
+                } else {
+                    yPos--;
+
+                    xPos++;
+                    direction = UP;
+                }
+            } else if (direction == UP) {
+                if (yPos >= 0 && spiral[yPos][xPos] == 0) {
+                    spiral[yPos][xPos] = filler++;
+                    yPos--;
+                } else {
+                    xPos++;
+
+                    yPos++;
+                    direction = RIGHT;
+                }
+            }
+        }
+        return spiral;
     }
 }
