7c4ba12644848410ed09c279f1f802e8ee36234e
michaelwm <michael@miduras.com>
2016-04-30 13:44:01 -0400

    day 11, tried arraylist but got time error (took over 10s), turned to native arrays and decreased time to 5s but now getting out of memory on codewars, going to try something else

--- src/com/michaelmidura/kataday/day011/Line.java
+++ src/com/michaelmidura/kataday/day011/Line.java
@@ -3,6 +3,27 @@ package com.michaelmidura.kataday.day011;
 public class Line {
 
     public static String WhoIsNext(String[] names, int n) {
-        return "";
+        int currentName = 0;
+
+        int[] currentIteration = {0, 1, 2, 3, 4};
+        int[] nextIteration = new int[currentIteration.length * 2];
+
+        int i, displacement;
+        while (n > 0) {
+            i = 0;
+            displacement = 0;
+            for (; i < currentIteration.length; i++) {
+                currentName = currentIteration[i];
+                nextIteration[i + displacement] = currentIteration[i];
+                nextIteration[i + 1 + displacement] = currentIteration[i];
+                displacement++;
+                n--;
+                if (n <= 0)
+                    break;
+            }
+            currentIteration = nextIteration;
+            nextIteration = new int[currentIteration.length * 2];
+        }
+        return names[currentName];
     }
 }
