e13d03697ab23d1d117c9335102c985f5ee2c6b7
michaelwm <michael@miduras.com>
2016-04-30 14:08:01 -0400

    found a pattern and solved!

--- src/com/michaelmidura/kataday/day011/Line.java
+++ src/com/michaelmidura/kataday/day011/Line.java
@@ -3,27 +3,12 @@ package com.michaelmidura.kataday.day011;
 public class Line {
 
     public static String WhoIsNext(String[] names, int n) {
-        int currentName = 0;
-
-        int[] currentIteration = {0, 1, 2, 3, 4};
-        int[] nextIteration = new int[currentIteration.length * 2];
-
-        int i, displacement;
-        while (n > 0) {
-            i = 0;
-            displacement = 0;
-            for (; i < currentIteration.length; i++) {
-                currentName = currentIteration[i];
-                nextIteration[i + displacement] = currentIteration[i];
-                nextIteration[i + 1 + displacement] = currentIteration[i];
-                displacement++;
-                n--;
-                if (n <= 0)
-                    break;
-            }
-            currentIteration = nextIteration;
-            nextIteration = new int[currentIteration.length * 2];
+        int nextIteration = 5, iterations = 1;
+        while (n - nextIteration > 0) {
+            n -= nextIteration;
+            nextIteration *= 2;
+            iterations *= 2;
         }
-        return names[currentName];
+        return names[(n - 1) / iterations];
     }
 }
