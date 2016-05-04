96c8a1ee7e505e082aca79c8db8ccdc8058c0cdc
michaelwm <michael@miduras.com>
2016-05-04 17:29:28 -0400

    int fix

--- src/com/michaelmidura/kataday/day015/Movie.java
+++ src/com/michaelmidura/kataday/day015/Movie.java
@@ -2,7 +2,7 @@ package com.michaelmidura.kataday.day015;
 
 public class Movie {
 
-    public static int movie(double card, double ticket, double perc) {
+    public static int movie(int card, int ticket, double perc) {
         double count = 0, systemB = card;
         while (ticket * count <= Math.ceil(systemB)) systemB += ticket * Math.pow(perc, ++count);
         return (int) count;
