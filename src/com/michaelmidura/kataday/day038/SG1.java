edc1e135c47b3cb898d451698c0d0d3d5025ad4f
michaelwm <michael@miduras.com>
2016-05-25 22:16:37 -0400

    learning how to implement A*

--- src/com/michaelmidura/kataday/day038/SG1.java
+++ src/com/michaelmidura/kataday/day038/SG1.java
@@ -30,23 +30,16 @@ public class SG1 {
         open.add(start);
 
         while (!open.isEmpty()) {
-            System.out.println("---");
-            Collections.sort(open);
-            Node current = open.get(0);
-            //System.out.println(current.x);
-            //System.out.println(current.y);
+            Node current = getSmallestF(open);
             open.remove(current);
+
             ArrayList<Node> successors = new ArrayList<>();
             for (int x = current.x - 1; x <= current.x + 1; x++)
-                for (int y = current.y - 1; y <= current.y + 1; y++) {
-                    if (x != current.x && y != current.y && x >= 0 && y >= 0 && x < nodes.length && y < nodes.length)
+                for (int y = current.y - 1; y <= current.y + 1; y++)
+                    if (!(x == current.x && y == current.y) && x >= 0 && y >= 0 && x < nodes.length && y < nodes.length && !nodes[x][y].blocked)
                         successors.add(nodes[x][y]);
-                }
 
-            System.out.println(successors.size());
             for (Node successor : successors) {
-                successor.parent = current;
-
                 if (successor.x == goal.x && successor.y == goal.y)
                     return "Found goal!";
 
@@ -54,24 +47,23 @@ public class SG1 {
                 successor.h = distance(successor, goal);
                 successor.f = successor.g + successor.h;
 
-                Collections.sort(open);
-                Collections.sort(closed);
-
-                if (!open.isEmpty() && open.get(0).x == successor.x && open.get(0).y == successor.y && open.get(0).f < successor.f)
-                    continue;
-                else if (!closed.isEmpty() && closed.get(0).x == successor.x && closed.get(0).y == successor.y && closed.get(0).f < successor.f)
-                    continue;
+                Node smallestOpen = getSmallestF(open);
+                Node smallestClosed = getSmallestF(closed);
+                if (smallestOpen != null && smallestOpen.x == successor.x && smallestOpen.y == successor.y)
+                    System.out.println("lol");
+                else if (smallestClosed != null && smallestClosed.x == successor.x && smallestClosed.y == successor.y)
+                    System.out.println("lol");
                 else
                     open.add(successor);
             }
+
             closed.add(current);
         }
 
         return "Oh for crying out loud...";
     }
 
-    public static class Node implements Comparable<Node> {
-        public Node parent;
+    public static class Node {
         public boolean blocked;
         public int x, y;
         public float f, g, h;
@@ -80,11 +72,9 @@ public class SG1 {
             this.x = x;
             this.y = y;
             this.blocked = blocked;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return Math.round(f - o.f);
+            f = 0;
+            g = 0;
+            h = 0;
         }
     }
 
@@ -92,5 +82,16 @@ public class SG1 {
         float dx = Math.abs(node1.x - node2.x);
         float dy = Math.abs(node1.y - node2.y);
         return (float) Math.sqrt((dx * dx) * (dy * dy));
+    }
+
+    public static Node getSmallestF(ArrayList<Node> nodes) {
+        Node smallestF = null;
+        if (!nodes.isEmpty()) {
+            smallestF = nodes.get(0);
+            for (Node node : nodes)
+                if (node.f < smallestF.f)
+                    smallestF = node;
+        }
+        return smallestF;
     }
 }
