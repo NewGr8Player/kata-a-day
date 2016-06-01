261c63d0c5859e84c2820b8561f15988c018c240
michaelwm <michael@miduras.com>
2016-06-01 10:05:01 -0400

    day 46

---
+++ src/com/michaelmidura/kataday/day046/WhichAreIn.java
@@ -0,0 +1,10 @@
+package com.michaelmidura.kataday.day046;
+
+import java.util.Arrays;
+
+public class WhichAreIn {
+
+    public static String[] inArray(String[] array1, String[] array2) {
+        return Arrays.stream(array1).filter(str -> Arrays.stream(array2).anyMatch(s -> s.contains(str))).distinct().sorted().toArray(String[]::new);
+    }
+}
