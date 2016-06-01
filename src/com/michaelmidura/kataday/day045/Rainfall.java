f2d2e8a110d7ed3c0a4a4bf01585796309f3cab4
michaelwm <michael@miduras.com>
2016-06-01 16:45:56 -0400

    one liners

--- src/com/michaelmidura/kataday/day045/Rainfall.java
+++ src/com/michaelmidura/kataday/day045/Rainfall.java
@@ -6,20 +6,21 @@ import java.util.Arrays;
 public class Rainfall {
 
     public static double mean(String town, String strng) {
-        for (String piece : strng.split("\n"))
-            if (piece.substring(0, piece.indexOf(":")).equals(town))
-                return Arrays.stream(piece.replaceAll("(?=[^0-9])(?=[^\\.])[^\\,]", "").split(",")).map(String::trim).mapToDouble(Double::parseDouble).sum() / 12;
-        return -1;
+        try {
+            return Arrays.stream(Arrays.stream(strng.split("\n")).filter(piece -> piece.substring(0, piece.indexOf(":")).equals(town)).findFirst().toString().replaceAll("(?=[^0-9])(?=[^\\.])[^\\,]", "").split(",")).map(String::trim).mapToDouble(Double::parseDouble).sum() / 12;
+        } catch (Exception e) {
+            return -1;
+        }
     }
 
     public static double variance(String town, String strng) {
         double mean = mean(town, strng);
         ArrayList<Double> variance = new ArrayList<>();
-        for (String piece : strng.split("\n"))
-            if (piece.substring(0, piece.indexOf(":")).equals(town))
-                Arrays.stream(piece.replaceAll("(?=[^0-9])(?=[^\\.])[^\\,]", "").split(",")).map(String::trim).mapToDouble(Double::parseDouble).forEach((n) -> variance.add((n - mean) * (n - mean)));
-        if (variance.isEmpty())
+        try {
+            Arrays.stream(Arrays.stream(strng.split("\n")).filter(piece -> piece.substring(0, piece.indexOf(":")).equals(town)).findFirst().toString().replaceAll("(?=[^0-9])(?=[^\\.])[^\\,]", "").split(",")).map(String::trim).mapToDouble(Double::parseDouble).forEach((n) -> variance.add((n - mean) * (n - mean)));
+        } catch (Exception e) {
             return -1;
-        return variance.stream().mapToDouble(d->d).sum() / 12;
+        }
+        return variance.stream().mapToDouble(d -> d).sum() / 12;
     }
 }
